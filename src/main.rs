0316241d414c6782a1935ee81c0a607303261fd7
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-01-28 23:20:08 +0900

    working example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::process::exit;
+use rich_sdl2_rust::{delay, EventBox, Sdl, Video};
+use rich_sdl2_rust::color::Rgb;
+use rich_sdl2_rust::geo::Rect;
+use rich_sdl2_rust::renderer::pen::Pen;
+use rich_sdl2_rust::renderer::Renderer;
+use rich_sdl2_rust::window::WindowBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = Sdl::new();
+    let video = Video::new(&sdl);
+    let window = WindowBuilder::builder()
+        .width(300)
+        .height(300)
+        .resizable(true)
+        .title("Hello, World!".to_string())
+        .build()
+        .new_window(&video);
+    let renderer = Renderer::new(&window);
+
+    macro_rules! use_pen {
+        ($closure:expr) => {
+            const fn assert<T: FnOnce(&Pen)>(_: &T) {}
+
+            assert(&$closure);
+            let pen = Pen::new(&renderer);
+            $closure(&pen);
+            drop(pen);
+        };
+    }
+
+    let mut event_box = EventBox::new(&video);
+    event_box.handle_quit(Box::new(|x| {
+        println!("exit");
+        exit(0)
+    }));
+
+    use_pen!(|pen: &Pen| {
+        pen.set_color(Rgb::from(0xFFFFFF));
+        pen.fill_rect(Rect::from_xs_ys([0, 100], [0, 100]));
+    });
+
+    window.show();
+    loop {
+        event_box.poll();
+        use_pen!(|pen: &Pen| {
+            pen.set_color(Rgb::from(0xFF0000));
+            pen.fill_rect(Rect::from_xs_ys([0, 100], [0, 100]));
+        });
+        delay(16);
+    }
 }
